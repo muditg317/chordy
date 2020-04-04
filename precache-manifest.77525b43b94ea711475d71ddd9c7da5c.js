6318b3eb314b63e3141c0abe07a51a46ab5251e6
Mudit Gupta <muditg317@gmail.com>
2020-04-04 15:14:59 -0400

    Updates

---
+++ precache-manifest.77525b43b94ea711475d71ddd9c7da5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1bdd1d8f131958a4114b013e224378",
+    "url": "/chordy/index.html"
+  },
+  {
+    "revision": "d72598d1c2aaa1138c61",
+    "url": "/chordy/static/css/main.b3c8cece.chunk.css"
+  },
+  {
+    "revision": "ce0bb57e902045aac001",
+    "url": "/chordy/static/js/2.6aa3ff17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chordy/static/js/2.6aa3ff17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72598d1c2aaa1138c61",
+    "url": "/chordy/static/js/main.7721784d.chunk.js"
+  },
+  {
+    "revision": "1fa9ef7cafacd8c24cfe",
+    "url": "/chordy/static/js/runtime-main.dbae0500.js"
+  },
+  {
+    "revision": "2d46fc74baa1d9007f22a705561c0044",
+    "url": "/chordy/static/media/logo.2d46fc74.svg"
+  }
+]);
